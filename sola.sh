7d1172e2f98c1877e8d70a9986282f591712756d
zjw0231 <114279968+zjw0231@users.noreply.github.com>
2024-02-09 23:01:22 +0800

    Update sola.sh

--- sola.sh
+++ sola.sh
@@ -19,6 +19,6 @@ sleep 5
 echo "---------------------------"
 
 # 创建一个新的screen会话并在其中运行sh ./mk命令
-screen -dmS zjw sh -c './mk; echo "mk finished"; sleep 5; ./run; echo "run finished"; exec bash'
+screen -dmS zjw sh -c 'sh ./mk; echo "mk finished"; sleep 5; sh ./run; echo "run finished"; exec bash'
 
 echo "Screen session 'zjw' started, running mk and run scripts."
