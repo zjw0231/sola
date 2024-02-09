8c59b3e1127815eb63870893828b0bec6d4423a8
zjw0231 <114279968+zjw0231@users.noreply.github.com>
2024-02-09 22:28:10 +0800

    Create sola.sh

---
+++ sola.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Update package list and install necessary packages
+
+sudo apt-get install screen
+sleep 5
+echo "---------------------------"
+
+git clone https://github.com/bolabola/sola.git
+sleep 5
+echo "---------------------------"
+
+cd sola
+sleep 5
+echo "---------------------------"
+
+sh ./mk
+sleep 5
+echo "---------------------------"
+
+screen -S zjw
+sleep 10
+echo "---------------------------"
+
+sh ./run
+sleep 5
